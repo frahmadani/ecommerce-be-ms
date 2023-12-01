c00535e31361fc5974e036d71e344a2caea94acc
frahmadani <frahmadani@gmail.com>
2023-12-01 08:09:18 +0700

    add app logger to logger

--- user/src/index.js
+++ user/src/index.js
@@ -4,6 +4,7 @@ const { PORT } = require('./config');
 const { databaseConn } = require('./database');
 const expressApp = require('./express-app');
 const { CreateChannel } = require('./utils');
+const logger = require('./utils/app-logger');
 
 const StartServer = async () => {
 
@@ -17,6 +18,7 @@ const StartServer = async () => {
 
     app.listen(PORT, () => {
         console.log(`User service listening on port ${PORT}`);
+        logger.info(`User service listening on port ${PORT}`);
     })
         .on('error', (err) => {
             console.log(err);
